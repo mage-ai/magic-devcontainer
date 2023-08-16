297b9c6072b746f5c6afd2a4bf239db8a1b4372b
Matt Palmer <mattppalmer@gmail.com>
2023-08-16 10:13:24 -0600

    arch diagram

---
+++ mingrammer/diagram.py
@@ -0,0 +1,30 @@
+from diagrams import Diagram, Cluster
+from diagrams.custom import Custom
+from diagrams.aws.storage import S3
+from diagrams.onprem.analytics import Dbt
+from diagrams.onprem.container import Docker
+from diagrams.onprem.queue import Kafka
+from diagrams.onprem.database import Postgresql
+from diagrams.programming.language import Python
+from diagrams.oci.database import Stream
+
+
+with Diagram(filename="magic-devcontainer", direction="LR"):
+  s3 = S3("S3")
+  with Cluster("devcontainer"):
+    with Cluster("Docker"):
+      kafka = Kafka("Kafka")
+      db = Postgresql("Postgres")
+      python = Python("Producer")
+      taxi = Custom("Taxi data", "./assets/car.png")
+      with Cluster("Mage 🧙🏻‍♂️"):
+        batch = Dbt("Batch pipeline")
+        stream = Stream("Streaming pipeline")
+
+  [taxi, kafka] >> python
+  python >> stream >> s3
+  s3 >> batch >> db
+
+        
+        
+        
